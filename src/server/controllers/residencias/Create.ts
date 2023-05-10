367462f586b104d88557cc7c413a125284904891
danielccosta9 <danielc.costa9@gmail.com>
2023-05-10 18:06:35 -0300

    feat: Adicionando o Yup, para validacoes.

--- src/server/controllers/residencias/Create.ts
+++ src/server/controllers/residencias/Create.ts
@@ -1,11 +1,34 @@
 import { Request, Response } from 'express';
-
+import { StatusCodes } from 'http-status-codes';
+import * as yup from 'yup';
 
 interface IResidencia {
     nome: string;
+    endereco: string;
 }
 
-export const create = (req: Request<{}, {}, IResidencia>, res: Response) => {
+const bodyValidationSchema: yup.Schema<IResidencia> = yup.object().shape({
+    nome: yup.string().required().min(3),
+    endereco: yup.string().required().min(3),
+});
+
+export const create = async (req: Request<{}, {}, IResidencia>, res: Response) => {
+
+    let validateData: IResidencia | undefined = undefined;
+
+    try {
+        validateData = await bodyValidationSchema.validate(req.body, { abortEarly: false });
+    } catch (err) {
+        const yupError = err as yup.ValidationError;
+        const errors: Record<string, string> = {};
+
+        yupError.inner.forEach((error) => {
+            if (error.path === undefined) return;
+            errors[error.path] = error.message;
+        });
+
+        return res.status(StatusCodes.BAD_REQUEST).json({ errors });
+    }
 
     console.log(req.body.nome);
     
