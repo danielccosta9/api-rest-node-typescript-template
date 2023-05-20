2efdba905cfa06b624637ff8e01fce24025f473a
danielccosta9 <danielc.costa9@gmail.com>
2023-05-20 15:43:02 -0300

    feat: corrigindo erro de producao

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
 import { server } from './server/Server';
+import { Knex } from './server/database/knex';
 
+const startServer = () => {
+    server.listen(process.env.PORT || 3330, () => {
+        console.log(`Server run in port ${process.env.PORT || 3330}`);
+    });
+};
 
-server.listen(process.env.PORT || 3330, () => console.log(`Server run in port ${process.env.PORT || 3330}`));
+if (process.env.IS_LOCALHOST !== 'true') {
+    Knex.migrate.latest().then(() => {
+        startServer();
+    }).catch(console.log);
+} else {
+    startServer();
+}
