e1d68668ac6b6dd2ad5f69cc17a89343a53ce0bf
danielccosta9 <danielc.costa9@gmail.com>
2023-05-22 18:48:29 -0300

    feat: resolvendo problema de producao

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
+import { Knex } from './server/database/knex';
 import { server } from './server/Server';
 
+const startServer = () => {
+    server.listen(process.env.PORT || 3333, () => {
+        console.log(`App rodando na porta ${process.env.PORT || 3333}`);
+    });
+};
 
-server.listen(process.env.PORT || 3330, () => console.log(`Server run in port ${process.env.PORT || 3330}`));
+
+if (process.env.IS_LOCALHOST !== 'true') {
+    Knex.migrate.latest()
+        .then(() => {
+            startServer();
+        })
+        .catch(console.log);
+} else {
+    startServer();
+}
