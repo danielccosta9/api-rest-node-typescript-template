7166b2a74e74f9b36b1902008b5a0e2ffd05217e
danielccosta9 <danielc.costa9@gmail.com>
2023-05-23 23:07:11 -0300

    feat: adicionando o seed

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,15 @@ const startServer = () => {
 };
 
 
-if (process.env.IS_LOCALHOST === 'true') {
-    Knex.migrate.latest().then(() => {
-        startServer();
-    });
+if (process.env.IS_LOCALHOST !== 'true') {
+    Knex.migrate
+        .latest()
+        .then(() => {
+            Knex.seed.run()
+                .then(() => startServer())
+                .catch(console.log);
+        })
+        .catch(console.log);
 } else {
     startServer();
 }
