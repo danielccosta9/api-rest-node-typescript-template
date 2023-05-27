ca429f321a9b536487e9f24a756ef4dd00894cfc
danielccosta9 <danielc.costa9@gmail.com>
2023-05-26 23:04:19 -0300

    feat: criando testes para pessoa

--- tests/pessoas/Create.test.ts
+++ tests/pessoas/Create.test.ts
@@ -17,7 +17,6 @@ describe('Pessoas - Create', () => {
     });
 
     it('Cria registro', async () => {
-
         const res1 = await testServer
             .post('/pessoas')
             .send({
@@ -31,8 +30,8 @@ describe('Pessoas - Create', () => {
         expect(res1.statusCode).toEqual(StatusCodes.CREATED);
         expect(typeof res1.body).toEqual('number');
     });
-    it('Tenta criar um registro com nome muito curto', async () => {
 
+    it('Tenta criar um registro com nome muito curto', async () => {
         const res1 = await testServer
             .post('/pessoas')
             .send({
@@ -45,5 +44,65 @@ describe('Pessoas - Create', () => {
 
         expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
         expect(res1.body).toHaveProperty('errors.body.nome');
+    });
+
+    it('Tenta criar um registro com cpf muito longo', async () => {
+        const res1 = await testServer
+            .post('/pessoas')
+            .send({
+                nome: 'pessoa',
+                cpf: '000.000.000-000',
+                nascimento: '10/10/2010',
+                telefone: '(83) 0000-0000',
+                residenciaId
+            });
+
+        expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+        expect(res1.body).toHaveProperty('errors.body.cpf');
+    });
+
+    it('Tenta criar um registro com cpf muito curto', async () => {
+        const res1 = await testServer
+            .post('/pessoas')
+            .send({
+                nome: 'pessoa',
+                cpf: '000.000.000-0',
+                nascimento: '10/10/2010',
+                telefone: '(83) 0000-0000',
+                residenciaId
+            });
+
+        expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+        expect(res1.body).toHaveProperty('errors.body.cpf');
+    });
+
+    it('Tenta criar um registro com telefone muito longo', async () => {
+        const res1 = await testServer
+            .post('/pessoas')
+            .send({
+                nome: 'pessoa',
+                cpf: '000.000.000-00',
+                nascimento: '10/10/2010',
+                telefone: '(83) 00000-00000',
+                residenciaId
+            });
+
+        expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+        expect(res1.body).toHaveProperty('errors.body.telefone');
+    });
+
+    it('Tenta criar um registro com telefone muito curto', async () => {
+        const res1 = await testServer
+            .post('/pessoas')
+            .send({
+                nome: 'pessoa',
+                cpf: '000.000.000-00',
+                nascimento: '10/10/2010',
+                telefone: '(83) 0000-000',
+                residenciaId
+            });
+
+        expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+        expect(res1.body).toHaveProperty('errors.body.telefone');
     });
 });
