cbdb7d130d8b1093847a308353c5e3deeb35fc99
danielccosta9 <danielc.costa9@gmail.com>
2023-05-29 19:45:50 -0300

    feat: adicionando JWT

--- src/server/shared/middleware/EnsureAuthenticated.ts
+++ src/server/shared/middleware/EnsureAuthenticated.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { JWTService } from '../services';
 
 
 export const ensureAuthenticated: RequestHandler = async (req, res, next) => {
@@ -22,12 +23,19 @@ export const ensureAuthenticated: RequestHandler = async (req, res, next) => {
         });
     }
 
-    if (token !== 'teste.teste.teste') {
+    const jwtData = JWTService.verify(token);
+
+    if (jwtData === 'JWT_SECRET_NOT_FOUND') {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            error: 'Erro ao verificar token de acesso'
+        });
+    } else if (jwtData === 'INVALID_TOKEN') {
         return res.status(StatusCodes.UNAUTHORIZED).json({
             error: 'Não autenticado!'
         });
     }
+    
+    req.headers.idUsuario = jwtData.uid.toString();
 
     return next();
-
 };
