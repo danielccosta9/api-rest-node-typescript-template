2a3d432506181106d31fe01a49bd59fb72c6a806
Daniel Costa <64346724+danielccosta9@users.noreply.github.com>
2023-05-30 00:29:42 -0300

    fix: ajustes no projeto

--- src/server/database/knex/Environment.ts
+++ src/server/database/knex/Environment.ts
@@ -36,11 +36,13 @@ export const production: Knex.Config = {
     directory: path.resolve(__dirname, '..', 'seeds'),
   },
   connection: {
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    database: process.env.DATABASE_NAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: Number(process.env.DATABASE_PORT || 5432),
-    ssl: { rejectUnauthorized: false },
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT || 5432),
+    ssl: {
+      rejectUnauthorized: false
+    }
   },
 };
